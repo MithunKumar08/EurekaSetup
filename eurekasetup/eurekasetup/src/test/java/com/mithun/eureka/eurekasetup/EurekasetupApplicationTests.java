5e9c3b9b5fd75ff750af099d5041ddcd5c028659
Kumar R <2375265@cognizant.com>
2025-09-17 21:30:18 +0530

    added all folders for eurekasetup service

---
+++ eurekasetup/eurekasetup/src/test/java/com/mithun/eureka/eurekasetup/EurekasetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mithun.eureka.eurekasetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekasetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
